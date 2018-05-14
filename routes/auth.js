d1fd03a9d31007113e5506a7dbf4a2fe5faff355
arjanmobin <arjanmobin@gmail.com>
2018-05-14 13:50:18 -0700

    authorization

--- routes/auth.js
+++ routes/auth.js
@@ -15,12 +15,74 @@ auth.post("/register",
   [
   body("username")
     .isLength({min: 4})
-    .withMessage("Username must be at least 4 characters")
+    .withMessage("Username must be at least 4 characters"),
+  body("password")
+    .isLength({min: 6})
+    .withMessage("Password must be at least 6 characters"),
+
+    .matches(/\d/)
+    .withMessage("Password must contain at least 1 number"),
 
   ],
 
   (req, res)=>{
-    
+    const errors = validationResult(req);
+    if(!(errors.isEmpty())){
+      const eMessages = errors.array().map(e =>{
+        return {message: e.msg};
+      });
+      req.flash("eMessages", eMessages);
+      res.redirect("/register");
+    }
+    else{
+      const uData = matchedData(req);
+      const user = new User(uData);
+      user.save()
+        .then((user)=>{
+          req.flash("sMessages", {message: "Resgistration Success!"});
+          res.redirect("/login");
+        })
+        .catch(e =>{
+          req.flash("eMessages", {message: "Registration Failed."});
+          if(e.code==11000){
+            req.flash("eMessages", {message: "Account already exists. Please login"});
+            res.redirect("/login");
+          }
+          else{
+            res.redirect("/register");
+          }
+        })
+    }
+});
+
+auth.get("/login", (req, res)=>{
+  res.render("login");
+});
+
+auth.post("/login", (req, res)=>{
+  User.find({email: req.body.email})
+    .then(user=>{
+      if(!user){
+        req.flash("eMessages", {message: "Account does not exist"});
+        res.redirect("/register");
+      }
+      else{
+        bcrype.compare(req.body.password, user.password)
+          .then(validPass=>{
+            if(validPass){
+              req.session.userId = user._id;
+              res.redirect("/home");
+            }
+            else{
+              req.flash("eMessages", {message: "Incorrect password"});
+              res.redirect("/login");
+            }
+          })
+          .catch(e=>{
+            req.flash("eMessages", {message: "An error occured loggin in"});
+          })
+      }
+    })
 })
 
 
